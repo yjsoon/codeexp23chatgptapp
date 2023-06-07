236bc193240c2ee0f4c671d61a68e07d881df56a
YJ Soon <yjsoon@tinkertanker.com>
2023-06-07 15:42:20 +0800

    Added insane bot functionality

--- App.js
+++ App.js
@@ -2,12 +2,63 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
 import { useState } from "react";
+import axios from "axios";
+
+const CHATGPT_API_URL = "https://api.openai.com/v1/completions";
+const OPENAI_KEY = "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";
+
+const generateResponse = async (text) => {
+  let response = null;
+  try {
+    response = await axios.post(
+      CHATGPT_API_URL,
+      {
+        model: "text-davinci-002",
+        prompt: `This is a conversation with an AI assistant. 
+    This AI assistant likes to talk in Hokkien and Singlish and 
+    always calls the user unker and is a bit grumpy. 
+    Human: ${text}
+    AI:`,
+        max_tokens: 150,
+        temperature: 2.0,
+        n: 1,
+        stop: "Human:"
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${OPENAI_KEY}`
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("We have a response");
+  console.log(response);
+  const { choices } = response.data;
+  const { text: generatedText } = choices[0];
+  return generatedText.trim();
+};
 
 export default function App() {
   const [messages, setMessages] = useState([]);
 
-  function onSend(newMessages) {
-    setMessages(GiftedChat.append(messages, newMessages));
+  async function onSend(newMessages) {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+    const reply = await generateResponse(newMessages);
+    const botReply = {
+      _id: Math.round(Math.random() * 100000),
+      text: reply,
+      createdAt: new Date(),
+      user: 2
+    };
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, botReply)
+    );
   }
 
   return (
